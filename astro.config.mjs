4d5a1c3aa6e1d0fa748197d8565898516092c58f
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2025-07-05 16:08:16 +0900

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), tailwind()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,12 @@
-import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      pubDate: z.coerce.date(),
-      updatedDate: z.coerce.date().optional(),
-    }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
 });
 
 export const collections = { blog };
